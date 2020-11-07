fafcc209658b1b06eb7795effc2905e7ab2b0f45
Kristen Bair <kristenmbair@gmail.com>
2020-11-06 19:00:46 -0700

    Create player class framework

---
+++ src/player.js
@@ -0,0 +1,17 @@
+class Player {
+  constructor(id, token) {
+    this.id = id;
+    this.token = token;
+    this.wins = [];
+  }
+
+  saveWinsToStorage() {
+    var stringifiedWins = JSON.stringify(this.wins);
+    localStorage.setItem("player-wins", stringifiedWins)
+  }
+
+  retrieveWinsFromStorage() {
+    var retrieved = localStorage.getItem("player-wins");
+    var parsedWins = JSON.parse(retrieved);
+  }
+}
