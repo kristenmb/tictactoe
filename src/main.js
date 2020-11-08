b3699dca27600763d61478132006a36e6d7dc484
Kristen Bair <kristenmbair@gmail.com>
2020-11-07 17:59:44 -0700

    Add draw condition and gameboard reset functionality

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 var gameboardGrid = document.querySelector('.grid')
+var gamePlayDisplay = document.querySelector('h1');
+var boxes = document.querySelectorAll('.box');
 
 gameboardGrid.addEventListener('click', addMark)
 
@@ -10,7 +12,15 @@ var game = new Game(player1, player2);
 function addMark() {
   var boxClicked = event.target;
   if (boxClicked.innerHTML === '') {
-    boxClicked.innerHTML = `${game.activePlayer.token}`
+    boxClicked.innerHTML = `${game.activePlayer.token}`;
   }
+
+  game.clickCount++;
+  game.checkWinConditions();
+  game.checkDrawConditions();
   game.switchPlayer();
+}
+
+function updateWins() {
+  
 }
